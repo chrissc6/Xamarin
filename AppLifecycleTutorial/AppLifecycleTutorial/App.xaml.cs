3f41ac3cd19f8772c5d772b3be568d4bdf72e1e2
chris <christopherscaudill@gmail.com>
2020-04-06 21:28:12 -0400

    App Lifecycle

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLifecycleTutorial
+{
+    public partial class App : Application
+    {
+
+        const string displayText = "displayText";
+
+        public string DisplayText { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+            Console.WriteLine("Start*************************************************************");
+            if (Properties.ContainsKey(displayText))
+            {
+                DisplayText = (string)Properties[displayText];
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+            Console.WriteLine("Sleep*************************************************************");
+            Properties[displayText] = DisplayText;
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+            Console.WriteLine("Resume*************************************************************");
+        }
+
+
+    }
+}
