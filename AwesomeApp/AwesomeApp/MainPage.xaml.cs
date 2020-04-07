2178128e86ede7736549175b360823f40894fe1c
chris <christopherscaudill@gmail.com>
2020-04-06 21:16:51 -0400

    Pop-ups

--- AwesomeApp/AwesomeApp/MainPage.xaml.cs
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -185,5 +185,22 @@ namespace AwesomeApp
         {
             Monkey tappedItem = e.Item as Monkey;
         }
+
+        async void OnDisplayAlertButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "This is an alert.", "OK");
+        }
+
+        async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
+            Console.WriteLine("Save data: " + response);
+        }
+
+        async void OnDisplayActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Console.WriteLine("Action: " + action);
+        }
     }
 }
