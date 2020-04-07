c6e30809492d137b6c425cd3eb4149baa0e576de
chris <christopherscaudill@gmail.com>
2020-04-06 21:52:39 -0400

    Local Database2 sqlite-net-pcl NuGet package

--- LocalDatabaseTutorial/LocalDatabaseTutorial/App.xaml.cs
+++ LocalDatabaseTutorial/LocalDatabaseTutorial/App.xaml.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 namespace LocalDatabaseTutorial
 {
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
@@ -15,14 +30,17 @@ namespace LocalDatabaseTutorial
 
         protected override void OnStart()
         {
+            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
+            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
+            // Handle when your app resumes
         }
     }
 }
